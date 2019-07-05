f1adbb8ab3d19a23386ba815209481b5407eedd0
sylain <sylvain.noiry@hotmail.fr>
2019-07-05 22:37:22 +0200

    code v0.9.2
    - mcu ISO code v0.9.2
    - mcu link code v0.9.2

--- mcu_ISU.ino
+++ mcu_ISU.ino
@@ -15,9 +15,9 @@ HX711 scale1(6, 7);
 HX711 scale2(8, 9);
 const int npn = 10;
 const float calibration_factor1 = 22500;
-const float zero_factor1 = -123000;
+const float zero_factor1 = -125000;
 const float calibration_factor2 = 22500;
-const float zero_factor2 = -123000;
+const float zero_factor2 = -122000;
 float UbatBin; //valeur
 float UbatVol; //voltage
 float photoResistorBin; //valeur
@@ -30,12 +30,13 @@ int transmition = 0;
 long int sec;
 char incomingByte;
 int start;
-int receive;
 
 void setup() {
  pinMode(npn, OUTPUT);
  pinMode(wakePin, INPUT);
- 
+
+ Serial.begin(9600);
+ Serial.println("initialisation");
  HC12.begin(9600);
  
  dht.begin();
@@ -62,43 +63,73 @@ void setup() {
 
 void loop() {
   //preparation a la mise en veille et remise a zero variables
+   // Serial.println("extinction des instruments");
   digitalWrite(npn, LOW);
-  UbatBin = 0;
-  UbatVol = 3.75;
-  photoResistorBin = 0;
-  photoResistorVol = 4.25;
-  temperature = 20.24;
-  humidite = 19.56;
-  poids1 = 204.20;
-  poids2 = 109.50;
+ //   Serial.println("remise a 0 des variables");
+ float UbatBin = 0;
+ float UbatVol = 0;
+ float photoResistorBin = 0;
+ float photoResistorVol = 0;
+ float temperature = 0;
+ float humidite = 0;
+ float poids1 = 0;
+ float poids2 = 0;
   transmition = 0;
   //veille
+   // Serial.println("mise ne veille");
+    delay(500);
   attachInterrupt(0, wakeUp, LOW); //debut de la veille
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   detachInterrupt(0); //fin de la veille
+delay(1000);
   RTC.armAlarm(1, true);
   RTC.clearAlarm(1);
   RTC.alarmInterrupt(1, true); //rearmement alarme
-  delay(1);
+ delay(100);
+  //  Serial.println("sortie de veille");
   
   //allumage des instruments puis delais attente
   digitalWrite(npn, HIGH);
+//    Serial.println("allumage des instruments");
   delay(2000);
+ 
   
   // recuperation de toutes les donnees des capteurs
+//    Serial.println("debut des mesures");
   temperature = dht.readTemperature();
+//    Serial.print("temperature ");
+  //  Serial.println(temperature);
   humidite = dht.readHumidity();
+ //   Serial.print("humidité ");
+ //   Serial.println(humidite);
   UbatBin = analogRead(A3);
+  //  Serial.print("ubat ");
+  //  Serial.println(UbatBin);
   photoResistorBin = analogRead(A2);
+   // Serial.print("lum ");
+   // Serial.println(photoResistorBin);
   poids1 = scale1.get_units(), 10;
+   // Serial.print("poids1 ");
+   // Serial.println(poids1);
   poids2 = scale2.get_units(), 10;
-  
-  //traitement des donnees a traiter
-  UbatVol = voltage(UbatBin);
-  photoResistorVol = voltage(photoResistorBin); 
+   // Serial.print("poids2 ");
+   // Serial.println(poids2);
 
+    //Serial.println("fin des mesures");
+  //traitement des donnees a traiter
+    //Serial.println("debut du traitement");
+  UbatVol = voltage(UbatBin);
+   // Serial.print("ubatvol ");
+   // Serial.println(UbatVol);
+  photoResistorVol = voltage(photoResistorBin); 
+   // Serial.print("lumvol ");
+   // Serial.println(photoResistorVol);
+
+   // Serial.println("fin du traitement");
+delay(100);
   //envoie par HC12
-  while(transmition < 2){
+   // Serial.println("debut de la transmission");
+  while(transmition < 3){
   HC12.print("z");
   delay(10);
   HC12.print("a");
@@ -114,10 +145,25 @@ void loop() {
   HC12.print("f");
   HC12.print(photoResistorVol);
   HC12.print("g");
-  delay(10);
-  sec = millis();
+//  Serial.print("z");
+  //Serial.print("a");
+  //Serial.print(temperature);
+  //Serial.print("b");
+ // Serial.print(humidite);
+ // Serial.print("c");
+ // Serial.print(poids1);
+ // Serial.print("d");
+ // Serial.print(poids2);
+ // Serial.print("e");
+ // Serial.print(UbatVol);
+//  Serial.print("f");
+ // Serial.print(photoResistorVol);
+ // Serial.print("g   ");
+  delay(3000);
   transmition++;
   }
+  delay(100);
+ // Serial.println("fin de la transmission");
 }
 
 float voltage(int valeurBinaire)
--- mcu_link.ino
+++ mcu_link.ino
@@ -4,10 +4,11 @@
     SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
     SoftwareSerial ESP8266(5, 6);
 
-    String NomduReseauWifi = "Entrez le nom de votre Box ou point d'accès Wifi"; // Garder les guillements
-    String MotDePasse      = "Entrez le nom du mot de passe de votre Box ou point d'accès Wifi"; // Garder les guillements
-    String httpGet = "";
-    int taille;
+    String NomduReseauWifi = "Freebox-ABC6E7"; // Garder les guillements
+    String MotDePasse = "inferent-apologum-decircinat-licium"; // Garder les guillements
+   // String httpGet = "";
+    String taille;
+    String taille2;
     int hcend = 0;
     int variableWrite = 0;
     long int temps;
@@ -24,17 +25,25 @@
     void setup()
     {
       pinMode(mosfet, OUTPUT);
-      ESP8266.begin(9600);  
-      HC12.begin(9600);
+      digitalWrite(mosfet, HIGH);
       Serial.begin(9600);
-      envoieAuESP8266("AT+RST");
-      recoitDuESP8266(2000);
+      ESP8266.begin(115200); 
+      initESP8266();  
+      HC12.begin(9600);
+     // Serial.println("initialisation");
+     // delay(500);
+     // initESP8266();
     }
    
     void loop()
     {
+   //   Serial.println("allumage des instruments");
      digitalWrite(mosfet, HIGH); 
       //reception donnees de HC12
+ //     Serial.println("reception des donnees");
+      HC12.listen();
+      int hcend = 0;
+      long int temps = millis(); 
       while( (temps+600000) > millis() && hcend < 2){
         while (HC12.available()) {
           incomingByte = HC12.read();
@@ -73,67 +82,133 @@
          else {
             switch (variableWrite) {
              case 1:
-                temperature += incomingByte;
+                 temperature += incomingByte;
               break; 
              case 2:
-                humidite += incomingByte;
+                 humidite += incomingByte;
               break; 
              case 3:
-                poids1 += incomingByte;
+                 poids1 += incomingByte;
               break; 
              case 4:
-                poids2 += incomingByte;
+                 poids2 += incomingByte;
               break; 
              case 5:
-                voltage += incomingByte;
+                 voltage += incomingByte;
               break; 
              case 6:
-                luminosite += incomingByte;
+                 luminosite += incomingByte;
               break; 
              default:  
               break;   
             }
          }
            }
-           hcend++;
+          
    }
-  
-      //Serial.print("temperature");
-     // Serial.print(temperature);
-     // Serial.print("humidite");
-     // Serial.print(humidite);
-     // Serial.print("poids1");
-     // Serial.print(poids1);
-     // Serial.print("poids2");
-     // Serial.print(poids2);
-      //Serial.print("voltage");
-     // Serial.print(voltage);
-     // Serial.print("luminosite");
-     // Serial.print(luminosite);
-     
+  /*
+      Serial.print("temperature");
+      Serial.println(temperature);
+      Serial.print("humidite");
+      Serial.println(humidite);
+      Serial.print("poids1");
+      Serial.println(poids1);
+      Serial.print("poids2");
+      Serial.println(poids2);
+      Serial.print("voltage");
+      Serial.println(voltage);
+      Serial.print("luminosite");
+      Serial.println(luminosite);
+    */ 
       //envoi par wifi
-      httpGet = String("GET /emoncms/input/post?node=emontx&fulljson={\"poidsruche1\":" + poids1 + ",\"poidsruche2\":" + poids2 + ",\"temperature\":" + temperature + ",\"humidite\":" + humidite + ",\"luminosite\":" + luminosite + ",\"voltagebatterie\":" + voltage + "}&apikey=827120f283dcac11ff6094a86daa0995&HTTP/1.1");
-      taille = httpGet.length() + 4;
-      Serial.println(httpGet);
-      initESP8266();
+      float po1 = poids1.toFloat();
+      float te = temperature.toFloat();
+      float hu = humidite.toFloat();
+      float po2 = poids2.toFloat();
+      float vo = voltage.toFloat();
+      float lu = luminosite.toFloat();
+
+    /*  String temp = String(te, DEC);
+      String hum = String(hu, DEC);
+      String p1 = String(po1, DEC);
+      String p2 = String(po2, DEC);
+      String vol = String(vo, DEC);
+      String lum = String(lu, DEC); */
+   /*   String temp = "1";
+      String hum = "1";
+      String p1 = "1";
+      String p2 = "1";
+      String vol = "1";
+      String lum = "1"; */
+      
+      ESP8266.listen();
+      Serial.println("**********************************************************");
       envoieAuESP8266("AT+CWMODE=1");
       recoitDuESP8266(5000);
+      Serial.println("**********************************************************");
       envoieAuESP8266("AT+CWJAP=\""+ NomduReseauWifi + "\",\"" + MotDePasse +"\"");
-      recoitDuESP8266(15000);
+      recoitDuESP8266(10000);
+      Serial.println("**********************************************************");
+      Serial.println("**********************************************************");
+      Serial.println("***************** INITIALISATION TERMINEE ****************");
+      Serial.println("**********************************************************");
+    /*  String temp = String(temperature.toFloat());
+      String hum = String(humidite.toFloat());
+      String p1 = String(poids1.toFloat());
+      String p2 = String(poids2.toFloat());
+      String vol = String(voltage.toFloat());
+      String lum = String(luminosite.toFloat()); */
+      String temp = "1.05";
+      String hum = "1";
+      String p1 = "1";
+      String p2 = "1";
+      String vol = "1";
+      String lum = "1"; 
+    /*  String temp = String(te, DEC);
+      String hum = String(hu, DEC);
+      String p1 = String(po1, DEC);
+      String p2 = String(po2, DEC);
+      String vol = String(vo, DEC);
+      String lum = String(lu, DEC); */
+     
+  //    String test = "GET /emoncms/input/post?node=emontx&fulljson={\"poidsruche1\":" + poids1 + ",\"poidsruche2\":" + poids2 + ",\"temperature\":" + temperature + ",\"humidite\":" + humidite + ",\"luminosite\":" + luminosite + ",\"voltagebatterie\":" + voltage + "}&apikey=827120f283dcac11ff6094a86daa0995&HTTP/1.1";
+      String httpget = "GET /emoncms/input/post?node=emontx&fulljson={\"poidsruche1\":" + poids1 + ",\"poidsruche2\":" + poids2 + ",\"temperature\":" + temperature + "}&apikey=827120f283dcac11ff6094a86daa0995&HTTP/1.1";
+      String httpget2 = "GET /emoncms/input/post?node=emontx&fulljson={\"humidite\":" + humidite + ",\"luminosite\":" + luminosite + ",\"voltagebatterie\":" + voltage + "}&apikey=827120f283dcac11ff6094a86daa0995&HTTP/1.1";
+      delay(100);
+      taille = httpget.length() + 4;
+      taille2 = httpget.length() + 4;
+   //   Serial.println(httpget);
+      Serial.println(httpget);
+      Serial.println(httpget2);
+      delay(10);
       envoieAuESP8266("AT+CIPSTART=\"TCP\",\"78.200.224.32\",80");
       recoitDuESP8266(3000);
       envoieAuESP8266("AT+CIPSEND=" + taille);   
       recoitDuESP8266(1000);
-      envoieAuESP8266(httpGet);
+      delay(100);
+      envoieAuESP8266(httpget);
+      recoitDuESP8266(1000);
+      envoieAuESP8266("AT+CIPCLOSE");
+      recoitDuESP8266(2000);
+      envoieAuESP8266("AT+CIPSTART=\"TCP\",\"78.200.224.32\",80");
+      recoitDuESP8266(3000);
+      envoieAuESP8266("AT+CIPSEND=" + taille2);   
+      recoitDuESP8266(1000);
+      delay(100);
+      envoieAuESP8266(httpget2);
       recoitDuESP8266(1000);
       envoieAuESP8266("AT+CIPCLOSE");
       recoitDuESP8266(2000);
       envoieAuESP8266("AT+CWQAP");
-      recoitDuESP8266(1000); 
+      recoitDuESP8266(2000);
+      Serial.println("fin de l envoi");
       delay(100);
+      Serial.println("extinction des instruments");
+       delay(100);
       digitalWrite(mosfet, LOW);
 
     //remise à zero
+      Serial.println("remise a zero des variables");
     temperature = "";
     humidite = "";
     luminosite = "";
@@ -141,26 +216,41 @@
     poids1 = "";
     poids2 = "";
     hcend = 0;
-
+       delay(100);
     //mise en veille
-    for (i=0; i <= 412; i++){
+      Serial.println("mise en veille");
+    for (i=0; i <= 0; i++){
       delay(1);
-      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-   }
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+      delay(100);
+   
+      Serial.println("sortie de veille");
+      i = 0;
    delay(100); 
-    }
+  } 
+    } 
    
     void initESP8266()
     {  
+      Serial.println("**********************************************************");  
+      Serial.println("**************** DEBUT DE L'INITIALISATION ***************");
+      Serial.println("**********************************************************");  
       envoieAuESP8266("AT+RST");
       recoitDuESP8266(2000);
+      Serial.println("**********************************************************");
+      Serial.println("**********************************************************");
+      Serial.println("***************** INITIALISATION TERMINEE ****************");
+      Serial.println("**********************************************************");
+      Serial.println(""); 
     }
 
-    void envoieAuESP8266(String commande)
+   void envoieAuESP8266(String commande)
     {  
       ESP8266.println(commande);
     }
- 
+    /****************************************************************/
+    /*Fonction qui lit et affiche les messages envoyés par l'ESP8266*/
+    /****************************************************************/
     void recoitDuESP8266(const int timeout)
     {
       String reponse = "";
